13ff7e0126feea2a8eb628085d2619c9d04c24fc
rafael.gracioli.glo <rafael.gracioli@globalhitss.com.br>
2021-11-22 17:27:09 -0300

    test: componentes editar-produto e lista

--- src/app/pages/editar-produto/editar-produto.component.spec.ts
+++ src/app/pages/editar-produto/editar-produto.component.spec.ts
@@ -1,14 +1,46 @@
+import { HttpClientModule } from '@angular/common/http';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ActivatedRoute, convertToParamMap, Router } from '@angular/router';
+import { of } from 'rxjs';
 
 import { EditarProdutoComponent } from './editar-produto.component';
+import { EditarProdutoModule } from './editar-produto.module';
+
+class RouterStub {
+  url = '/editar-produto/33';
+  navigateByUrl(url: string) {
+    return url;
+  }
+
+  navigate(route: any) {
+    return null;
+  }
+}
 
 describe('EditarProdutoComponent', () => {
   let component: EditarProdutoComponent;
   let fixture: ComponentFixture<EditarProdutoComponent>;
+  let activatedRouteSpy;
 
   beforeEach(async(() => {
+    activatedRouteSpy = {
+      snapshot: {
+        paramMap: convertToParamMap({
+          code: 33
+        })
+      }
+    };
     TestBed.configureTestingModule({
-      declarations: [ EditarProdutoComponent ]
+      imports: [
+        EditarProdutoModule,
+        HttpClientModule,
+        BrowserAnimationsModule
+      ],
+      providers: [
+        {provide: Router, useClass: RouterStub},
+        {provide: ActivatedRoute, useValue: activatedRouteSpy}
+      ]
     })
     .compileComponents();
   }));
--- src/app/pages/lista/lista.component.spec.ts
+++ src/app/pages/lista/lista.component.spec.ts
@@ -1,6 +1,21 @@
+import { HttpClientModule } from '@angular/common/http';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Router } from '@angular/router';
 
 import { ListaComponent } from './lista.component';
+import { ListaModule } from './lista.module';
+
+class RouterStub {
+  url = '/lista';
+  navigateByUrl(url: string) {
+    return url;
+  }
+
+  navigate(route: any) {
+    return null;
+  }
+}
 
 describe('ListaComponent', () => {
   let component: ListaComponent;
@@ -8,7 +23,14 @@ describe('ListaComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ListaComponent ]
+      imports: [
+        ListaModule,
+        HttpClientModule,
+        BrowserAnimationsModule
+      ],
+      providers: [
+        {provide: Router, useClass: RouterStub}
+      ]
     })
     .compileComponents();
   }));
@@ -21,5 +43,12 @@ describe('ListaComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('#createProduct - Should navaget to cadastro-produto when it is called', () => {
+    const test = spyOn(component.router, 'navigate');
+    component.createProduct();
+
+    expect(test).toHaveBeenCalledWith(['cadastro-produto']);
   });
 });
