162b135773eee84c1cd3f3f5ff8268eb82f0d4ce
rafael.gracioli.glo <rafael.gracioli@globalhitss.com.br>
2021-11-22 11:19:26 -0300

    feat(Product): Criação do componente e reorganização das pastas

--- src/app/pages/lista/lista.module.ts
+++ src/app/pages/lista/lista.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListaComponent } from './lista.component';
+import { ProductReadComponent } from '../../components/product/product-read/product-read.component';
 import { LoginRoutingModule } from './lista-routing.module';
 
 
 
 @NgModule({
-  declarations: [ListaComponent],
+  declarations: [
+    ListaComponent,
+    ProductReadComponent
+  ],
   imports: [
     CommonModule,
     LoginRoutingModule
